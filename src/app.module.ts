b83e9b02a1cf1cba081b7108f5eaa60d1394aeb6
jhongxu <jhxu2392@outlook.com>
2023-11-02 23:12:41 +0800

    feat: add users roles

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { UsersModule } from './modules/users/users.module';
+import { RolesModule } from './modules/roles/roles.module';
 
 @Module({
-	imports: [],
+	imports: [UsersModule, RolesModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
